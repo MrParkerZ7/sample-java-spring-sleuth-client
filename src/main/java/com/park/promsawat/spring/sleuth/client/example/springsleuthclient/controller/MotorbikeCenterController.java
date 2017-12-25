2d70171ee7c9495677623cfb66becf33f20eec4f
Park <neverzero9@gmail.com>
2017-12-25 14:39:11 +0700

    full example

--- src/main/java/com/park/promsawat/spring/sleuth/client/example/springsleuthclient/controller/MotorbikeCenterController.java
+++ src/main/java/com/park/promsawat/spring/sleuth/client/example/springsleuthclient/controller/MotorbikeCenterController.java
@@ -2,6 +2,7 @@ package com.park.promsawat.spring.sleuth.client.example.springsleuthclient.contr
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,16 +10,25 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 @RestController
-@RequestMapping("/buy")
+@RequestMapping("/showroom/buy")
 public class MotorbikeCenterController {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(MotorbikeCenterController.class);
 
+    // Instant RestTemplate to
+    @Autowired
     private RestTemplate restTemplate;
 
     @GetMapping("/{bike}")
-    public String getbike(@PathVariable("bike") final String bike) {
-        restTemplate.getForObject("http://localhost:8085/factory/oder/" + bike, String.class);
-        return null;
+    public String getBike(@PathVariable("bike") final String bike) {
+
+        LOGGER.info("Before calling server");
+
+        // Get Object from url another server
+        String response = restTemplate.getForObject("http://localhost:8085/factory/order/" + bike, String.class);
+
+        LOGGER.info("After calling server");
+
+        return response; //return back
     }
 }
